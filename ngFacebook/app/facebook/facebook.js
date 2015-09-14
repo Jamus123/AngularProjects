bb94bd58297cd1d7682578c90881902eeb8fdcc4
James Collins <jactenors@gmail.com>
2015-09-13 19:49:31 -0700

    Not going to complete FB app because my profile is private

--- ngFacebook/app/facebook/facebook.js
+++ ngFacebook/app/facebook/facebook.js
@@ -1,60 +1,68 @@
 'use strict';
 
-angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
+angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
 
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/facebook', {
-        templateUrl: 'facebook/facebook.html',
-        controller: 'FacebookCtrl'
-    });
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
 }])
 
-.config(function($facebookProvider) {
-    $facebookProvider.setAppId('741742819263188');
-    $facebookProvider.setPermissions('email', 'public_profile',
-        'user_posts', 'publish_actions', 'user_photos');
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('741742819263188');
+  $facebookProvider.setPermissions("email,public_profile, user_posts, publish_actions, user_photos");
 })
 
-.run(function($rootScope) {
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+.run(function($rootScope){
+	(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 })
-
 
 .controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
-    $scope.isLoggedIn = false;
+	$scope.isLoggedIn = false;
 
-    $scope.login = function() {
-        $facebook.login().then(function() {
-            $scope.isLoggedIn = true;
-            refresh();
-        });
-    }
+	$scope.login = function(){
+		$facebook.login().then(function(){
+			$scope.isLoggedIn = true;
+			refresh();
+		});
+	}
 
-    $scope.logout = function() {
-        $facebook.login().then(function() {
-            $scope.isLoggedIn = false;
-            refresh();
-        });
-    }
+	$scope.logout = function(){
+		$facebook.logout().then(function(){
+			$scope.isLoggedIn = false;
+			refresh();
+		});
+	}
 
-    function refresh() {
-        $facebook.api("/me").then(function(response) {
-            $scope.welcomeMsg = "Welcome " + response.name + "!";
-            $scope.isLoggedIn = true;
-            $scope.userInfo = response;
-            console.log('userinfo', $scope.userInfo);
-        }, function(err) {
-            $scope.welcomeMsg = "Please Log In";
-        });
-    }
-    refresh();
+	function refresh(){
+		$facebook.api("/me").then(function(response){
+			$scope.welcomeMsg = "Welcome "+ response.name;
+			$scope.isLoggedIn = true;
+			$scope.userInfo = response;
+			console.log("$scope.userinfo", $scope.userInfo);
+			$facebook.api('/me/picture').then(function(response){
+				$scope.picture = response.data.url;
+				$facebook.api('/me/permissions').then(function(response){
+					$scope.permissions = response.data;
+					$facebook.api('/me/posts').then(function(response){
+						console.log(response.data);
+						$scope.posts = response.data;
+					});
+				});
+			});
+
+		},
+		function(err){
+			$scope.welcomeMsg = "Please Log In";
+		});
+	}
+
+	refresh();
 }]);
