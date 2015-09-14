3e49fa2005b8ac7272b014182eab6d52ad47b93d
James Collins <jactenors@gmail.com>
2015-09-13 19:31:40 -0700

    finished log-in and API data retrieval/display

---
+++ ngFacebook/app/facebook/facebook.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/facebook', {
+        templateUrl: 'facebook/facebook.html',
+        controller: 'FacebookCtrl'
+    });
+}])
+
+.config(function($facebookProvider) {
+    $facebookProvider.setAppId('741742819263188');
+    $facebookProvider.setPermissions('email', 'public_profile',
+        'user_posts', 'publish_actions', 'user_photos');
+})
+
+.run(function($rootScope) {
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+})
+
+
+.controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+    $scope.isLoggedIn = false;
+
+    $scope.login = function() {
+        $facebook.login().then(function() {
+            $scope.isLoggedIn = true;
+            refresh();
+        });
+    }
+
+    $scope.logout = function() {
+        $facebook.login().then(function() {
+            $scope.isLoggedIn = false;
+            refresh();
+        });
+    }
+
+    function refresh() {
+        $facebook.api("/me").then(function(response) {
+            $scope.welcomeMsg = "Welcome " + response.name + "!";
+            $scope.isLoggedIn = true;
+            $scope.userInfo = response;
+            console.log('userinfo', $scope.userInfo);
+        }, function(err) {
+            $scope.welcomeMsg = "Please Log In";
+        });
+    }
+    refresh();
+}]);
